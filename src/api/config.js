17f4f229340c8a4838ae23a9d817cb84b2dadb01
Ribbinnn <54407267+Ribbinnn@users.noreply.github.com>
2022-07-21 12:11:23 +0700

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
 /* LOCALHOST + DOCKER */
-export const serverURL = `http://ec2-18-207-217-239.compute-1.amazonaws.com:5000/api`;
+export const serverURL = `https://xns6xsqhec.execute-api.us-east-1.amazonaws.com` // api gateway
+// export const serverURL = `http://ec2-18-207-217-239.compute-1.amazonaws.com:5000/api`;
 // export const serverURL = `http://localhost:5000/api`;
-
