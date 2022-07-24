340262fb9f887f1f7707aa762f5b2707ef56ffe1
Ribbinnn <r.supichaya29@gmail.com>
2022-07-25 00:42:26 +0700

    about us: adjust css

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -5,7 +5,7 @@ function AboutUs() {
     return (
         <div className="content">
             <Row style={{ marginBottom: "5px" }}>
-                <Col xs={24} sm={10}>
+                {/* <Col xs={24} sm={10}>
                     <div style={{ fontSize: "30px", marginBottom: "17px" }}>
                         {`About Us`}
                     </div>
@@ -31,7 +31,7 @@ function AboutUs() {
                             height={55}
                             src="/logos/EngCU_Logo.jpg"
                         />
-                    </Row>
+                    </Row> */}
                     {/* <Row justify="end" style={{ marginRight: "10px" }}>
                         <Image
                             preview={false}
@@ -39,11 +39,11 @@ function AboutUs() {
                             src="/logos/EngCU_Logo.jpg"
                         />
                     </Row> */}
-                </Col>
-                <hr style={{ width: "100%" }} />
+                {/* </Col>
+                <hr style={{ width: "100%" }} /> */}
             </Row>
             <Row>
-                <Col xl={13} style={{ paddingRight: "10px" }}>
+                <Col xl={12} style={{ paddingRight: "10px" }}>
                     <Row align="middle" style={{ marginBottom: "8px", minHeight: "90px" }}>
                         <Image
                             className="MedLogo"
@@ -58,17 +58,17 @@ function AboutUs() {
                             src="/logos/CUNM_Logo.png"
                         />
                     </Row>
-                    <Row style={{ color: "#9772fb", marginBottom: "5px", fontSize: "16px", minHeight: "51px" }}>
+                    <Row style={{ color: "#9772fb", marginBottom: "8px", fontSize: "16px", minHeight: "51px" }}>
                         <Col span={24}>
                             ศูนย์เชี่ยวชาญเฉพาะทางด้านระบบประสาทและการเคลื่อนไหวของระบบทางเดินอาหาร <br />ภาควิชาอายุรศาสตร์ คณะแพทยศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย
                         </Col>
                     </Row>
-                    <Row style={{ fontSize: "14px", marginBottom: "18px", minHeight: "67px" }}>
+                    <Row style={{ fontSize: "14px", marginBottom: "30px", minHeight: "67px" }}>
                         <Col span={16}>
                             10th floor, Bhumisirimangalanusorn Building, <br />King Chulalongkorn Memorial Hospital, Rama 4 Road, Pathumwan, Pathumwan, Bangkok, 10330, Thailand
                         </Col>
                     </Row>
-                    <Row justify="space-around" style={{ marginBottom: "35px" }}>
+                    {/* <Row justify="space-around" style={{ marginBottom: "35px" }}>
                         <Col md={7} style={{ marginBottom: "20px" }}>
                             <Row justify="center">
                                 <Image
@@ -106,8 +106,51 @@ function AboutUs() {
                             </Row>
                         </Col>
                         <Col xs={0} sm={0} md={2} />
+                    </Row> */}
+                    <Row justify="space-around">
+                        <Col sm={12} md={8} style={{ marginBottom: "20px" }}>
+                            <Row justify="center">
+                                <Image
+                                    preview={false}
+                                    height={190}
+                                    src="/people/อ-สุเทพ-2-2-64.jpg"
+                                />
+                            </Row>
+                            <Row justify="center">
+                                ศ.นพ.สุเทพ กลชาญวิทย์
+                            </Row>
+                        </Col>
+                        <Col sm={12} md={8} style={{ marginBottom: "20px" }}>
+                            <Row justify="center">
+                                <Image
+                                    preview={false}
+                                    height={190}
+                                    src="/people/พ.ฐนิสา.jpg"
+                                />
+                            </Row>
+                            <Row justify="center">
+                                ผศ.(พิเศษ)พญ.ฐนิสา พัชรตระกูล
+                            </Row>
+                        </Col>
+                        <Col xs={0} sm={0} md={8} />
+                    </Row>
+                    <Row justify="space-around" /*style={{ marginBottom: "35px" }}*/>
+                        <Col sm={12} md={16} style={{ marginBottom: "20px" }}>
+                            <Row justify="center">
+                                <Image
+                                    preview={false}
+                                    height={190}
+                                    src="/people/พ.ภัคพล.jpg"
+                                />
+                            </Row>
+                            <Row justify="center">
+                                อ.นพ.ภัคพล รัตนชัยสิทธิ์
+                            </Row>
+                        </Col>
+                        <Col xs={0} sm={0} md={8} />
                     </Row>
                 </Col>
+                <Col xl={1} />
                 <Col xl={11} style={{ paddingRight: "10px" }}>
                     <Row align="middle" style={{ marginBottom: "8px", minHeight: "90px" }}>
                         <Image
@@ -117,12 +160,12 @@ function AboutUs() {
                             src="/logos/EngCU_Logo.jpg"
                         />
                     </Row>
-                    <Row style={{ color: "#9772fb", marginBottom: "5px", fontSize: "16px", minHeight: "51px" }}>
+                    <Row style={{ color: "#9772fb", marginBottom: "8px", fontSize: "16px", minHeight: "51px" }}>
                         <Col span={24}>
                             ภาควิชาวิศวกรรมคอมพิวเตอร์ คณะวิศวกรรมศาสตร์ จุฬาลงกรณ์มหาวิทยาลัย
                         </Col>
                     </Row>
-                    <Row style={{ fontSize: "14px", marginBottom: "18px", minHeight: "67px" }}>
+                    <Row style={{ fontSize: "14px", marginBottom: "30px", minHeight: "67px" }}>
                         <Col span={20}>
                             17th floor, Engineering 4 Building (Charoenvidsavakham), Phayathai Road, Wang Mai, Pathumwan, Bangkok, 10330, Thailand
                         </Col>
@@ -154,7 +197,7 @@ function AboutUs() {
                         </Col>
                         <Col xs={0} sm={0} md={8} />
                     </Row>
-                    <Row justify="space-around" style={{ marginBottom: "35px" }}>
+                    <Row justify="space-around" /*style={{ marginBottom: "35px" }}*/>
                         <Col sm={12} md={8} style={{ marginBottom: "20px" }}>
                             <Row justify="center">
                                 <Image
